6b427642de9a1d61a0057334612a510ce6ccd056
TZ.J <tzjeung@gmail.com>
2026-01-18 22:59:04 +0700

    fix cwe-798

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
 import { PrismaService } from '../prisma.service';
 import { AuthController } from './auth.controller';
 import { AppController } from './app.controller';
@@ -6,7 +9,19 @@ import { AuthService } from './auth.service';
 import { AppService } from './app.service';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController, AppController],
   providers: [AuthService, AppService, PrismaService],
+  exports: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService
+  ) { }
 
   async findAll() {
     return { message: 'This action returns all Auth' };
+  }
+
+  async login(user: any) {
+    const payload = { username: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 
   async create(data: any) {
